f55399e763131de09f2c62e5b756b5f207c33350
Yifeng Zhang <yifeng2019@gmail.com>
2026-06-21 23:48:17 +0000

    modify the gen.go and with build result

--- experiments/issue-1497/gen.go
+++ experiments/issue-1497/gen.go
@@ -3,6 +3,6 @@
 // Requires on Linux host: clang, llvm, libbpf-dev
 // Generated *_bpf*.go files should be committed so the experiment can build without clang.
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang -cflags "-I/usr/include/$(uname -m)-linux-gnu" probe ./probe.bpf.c
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang -target amd64 -cflags "-I." probe ./probe.bpf.c
 
 package main
--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.23
 
 require github.com/cilium/ebpf v0.17.3
 
+require golang.org/x/sys v0.30.0 // indirect
+
 // Uncomment to test against local cilium/ebpf changes:
 // replace github.com/cilium/ebpf => /Users/yifengzh/workspace/ebpf
