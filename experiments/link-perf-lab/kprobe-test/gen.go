08ffb6cce1bf8d19568d174c25aa8ac40e293efc
Yifeng Zhang <yifeng2019@gmail.com>
2026-06-23 18:17:42 +0000

    modify the bpf.c gen name

--- experiments/link-perf-lab/kprobe-test/gen.go
+++ experiments/link-perf-lab/kprobe-test/gen.go
@@ -3,6 +3,6 @@
 // Requires on Linux host: clang, llvm, libbpf-dev
 // Generated *_bpf*.go files should be committed so the experiment can build without clang.
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang -target amd64 -cflags "-I. -I../../.." probe ./file_tracepoint.bpf.c
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang -target amd64 -cflags "-I. -I../../.." fileTrace ./file_tracepoint.bpf.c
 
 package main
